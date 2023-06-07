d35bf3351de9621b3fae4179cdaa35cb73bfe91a
Giancarlo Huarcaya <giancarlohuarcaya@salesland.net>
2023-06-06 21:55:17 -0500

    add ddl script

---
+++ ddl.sql
@@ -0,0 +1,28 @@
+CREATE TABLE [dbo].[PermissionTypes](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Descripcion] [varchar](200) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+
+CREATE TABLE [dbo].[Permissions](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[NombreEmpleado] [varchar](200) NULL,
+	[ApellidoEmpleado] [varchar](200) NULL,
+	[TipoPermiso] [int] NULL,
+	[FechaPermiso] [datetime] NULL,
+ CONSTRAINT [PK_Permissions__Id] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Permissions]  WITH CHECK ADD FOREIGN KEY([TipoPermiso])
+REFERENCES [dbo].[PermissionTypes] ([ID])
+GO
